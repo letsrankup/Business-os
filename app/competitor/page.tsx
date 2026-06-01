cf2f734f3dce969596b7fa97e71e2b40f33d2286
letsrankup <zaheerrind043@gmail.com>
2026-06-02 00:28:50 +0500

    Create page.tsx

---
+++ app/competitor/page.tsx
@@ -0,0 +1,13 @@
+import CompetitorForm from "@/components/CompetitorForm";
+
+export default function CompetitorPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">
+        Competitor Analysis
+      </h1>
+
+      <CompetitorForm />
+    </div>
+  );
+}
